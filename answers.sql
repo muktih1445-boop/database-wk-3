9b0ab26dd68694498e14bbf0c309f43d5cc238ba
muktar - <muktih1445@gmail.com>
2025-09-10 15:46:35 +0300

    completed assignment. created table and modified it

--- answers.sql
+++ answers.sql
@@ -1,1 +1,17 @@
+use salesdb;
 
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+
+use salesdb.student
+ISERT INTO student (id, fullName, age) VALUES
+(1, 'Alice Johnson', 19)
+(2, 'Bob Smith', 21)
+(3, 'Charlie Brown', 22)
+
+UPDATE salesdb.student
+SET age = 20
+WHERE id = 2;
